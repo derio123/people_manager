1ad7fdf78e4f9859d439007799afee98a49e5979
derio123 <deriomatheus6@gmail.com>
2019-07-25 21:06:07 -0300

    derio123 Novo projeto de gerenciamento de pessoas.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'editar/:id', loadChildren: './views/editar/editar.module#EditarPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,80 @@
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  public colunas = [
+    {descricao: 'Nome'},
+    {descricao: 'Telefone'},
+    {descricao: 'Endereço'},
+    {descricao: 'Data'},
+    {descricao: 'Ações'},
+  ];
+
+  items: Item[] = [];
+ 
+  newItem: Item = <Item>{};
+ 
+  @ViewChild('mylist')mylist: IonList;
+ 
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+ 
+  // CREATE
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+ 
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast('Item added!')
+      this.loadItems(); // Or add it to the array directly
+    });
+  }
+ 
+  // READ
+  loadItems() {
+    this.storageService.getItems().then(items => {
+      this.items = items;
+    });
+  }
+ 
+  // UPDATE
+  updateItem(item: Item) {
+    item.name = `UPDATED: ${item.name}`;
+    item.modified = Date.now();
+ 
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('Item updated!');
+      this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+      this.loadItems(); // Or update it inside the array directlydeleteItem(item: Item)
+    });
+  }
+ 
+  // DELETE
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems(); // Fix or sliding is stuck afterwards
+      this.loadItems(); // Or splice it from the array directly
+    });
+  }
+ 
+  // Helper
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+}
